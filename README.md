be1cf3f0f9aca9e5851d325f2f3d26d1b0157a70
fabricio jacob <fabricio.jacob@outlook.com>
2023-01-10 23:02:16 -0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sqs
+
+https://docs.aws.amazon.com/pt_br/AWSSimpleQueueService/latest/SQSDeveloperGuide/welcome.html
---
+++ sqs-examples/src/test/java/com/github/fabriciolfj/sqsexamples/SqsExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabriciolfj.sqsexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
